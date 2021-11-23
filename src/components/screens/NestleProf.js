abd85956e31e8c45e1d811519c47e7bc82316e09
Yeison Serna <yeisonivanserna@gmail.com>
2021-11-23 17:28:04 -0500

    screen ya iniciada sesion

---
+++ src/components/screens/NestleProf.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Sidebar } from "../screens/Sidebar";
+
+export const NestleProf = () => {
+  return (
+    <div>
+      <h1>Nestle Professional</h1>
+      <Sidebar />
+    </div>
+  );
+};
